c55b714c714bab25f363ba099b4d13bf1853b108
Guilherme Fernandes <guifjj92@gmail.com>
2023-08-16 20:44:58 -0300

    feat: Server json-server

---
+++ server.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const jsonServer = require("json-server");
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.resolve(__dirname, "data", "db.json"));
+const middlewares = jsonServer.defaults();
+
+const port = 5000;
+
+server.use(middlewares);
+
+server.use(router);
+server.listen(port, () => {
+  console.log("JSON Server is running");
+});
